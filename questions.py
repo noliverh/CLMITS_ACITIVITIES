9ec55eeda61e4a247b6849958822d472fd5d50b4
noliverh <nlvrheredia@gmail.com>
2021-07-31 14:07:26 +0800

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,45 @@
+'''
+This is the list of my questions to be imported on my quiz_game.py
+'''
+list_quiz = {
+    1 : {
+        "question" : "What is the Capital City of Japan?",
+        "answer" : "Tokyo"
+    },
+    2 : {
+        "question" : "What is the Capital City of Italy?",
+        "answer" : "Rome"
+    },
+    3 : {
+        "question" : "What is the Capital City of Australia?",
+        "answer" : "Canberra"
+    },
+    4 : {
+        "question" : "What is the Capital City of Bahrain?",
+        "answer" : "Manama"
+    },
+    5 : {
+        "question" : "What is the Capital City of Belgium?",
+        "answer" : "Brussels"
+    },
+    6 : {
+        "question" : "What is the Capital City of Egypt?",
+        "answer" : "Cairo"
+    },
+    7 : {
+        "question" : "What is the Capital City of Denmark?",
+        "answer" : "Copenhagen"
+    },
+    8 : {
+        "question" : "What is the Capital City of Greece?",
+        "answer" : "Athens"
+    },
+    9 : {
+        "question" : "What is the Capital City of India?",
+        "answer" : "New Delhi"
+    },
+    10 : {
+        "question" : "What is the Capital City of Maldives?",
+        "answer" : "Male"
+    }
+}
